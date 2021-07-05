836b20f6e7e30ea30685e85d331e316a2e203e8b
Umang Mittal <umangm99@gmail.com>
2021-07-05 21:43:46 +1000

    Adding file name in lambda

--- api-gateway/http-presigned-upload-url/lambda/pre-signed-upload-url/index.js
+++ api-gateway/http-presigned-upload-url/lambda/pre-signed-upload-url/index.js
@@ -4,8 +4,9 @@ const s3 = new AWS.S3()
 exports.handler = async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   console.log("CONTEXT: \n" + JSON.stringify(context, null, 2));
+  var file = event.queryStringParameters['file'] ? event.queryStringParameters['file'] : Date.now();
   var contentType = event.headers['content-type'] !== undefined ? event.headers['content-type'] : 'application/octet-stream';
   var bucket = process.env.S3_BUCKET
-  var params = {Bucket: bucket, Key: 'key.png', ContentType: contentType,};
+  var params = {Bucket: bucket, Key: file, ContentType: contentType,};
   return s3.getSignedUrl('putObject', params);
 }
