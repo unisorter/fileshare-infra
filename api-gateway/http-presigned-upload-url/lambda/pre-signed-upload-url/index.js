e696703e0fb4a1cdbdce74dfcdde0d36c576caaf
Umang Mittal <umangm99@gmail.com>
2021-07-05 18:21:08 +1000

    Adding content type check

--- api-gateway/http-presigned-upload-url/lambda/pre-signed-upload-url/index.js
+++ api-gateway/http-presigned-upload-url/lambda/pre-signed-upload-url/index.js
@@ -4,7 +4,10 @@ const s3 = new AWS.S3()
 exports.handler = async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   console.log("EVENT: \n" + JSON.stringify(context, null, 2));
-  let bucket = process.env.S3_BUCKET
-  var params = {Bucket: bucket, Key: 'key.png', ContentType: 'image/png',};
-  return s3.getSignedUrl('putObject', params);
+  var contentType = event.headers['content-type'] !== undefined ? event.headers['content-type'] : 'application/octet-stream';
+  var bucket = process.env.S3_BUCKET
+  var params = {Bucket: bucket, Key: 'key.png', ContentType: contentType,};
+  var presignedUrl = s3.getSignedUrl('putObject', params);
+  console.log(presignedUrl);
+  return presignedUrl;
 }
