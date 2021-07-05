e4b900941b6b96ebeb81f1c1c5cd7787a5dcca5d
Umang Mittal <umangm99@gmail.com>
2021-07-05 18:29:32 +1000

    Removed response log

--- api-gateway/http-presigned-upload-url/lambda/pre-signed-upload-url/index.js
+++ api-gateway/http-presigned-upload-url/lambda/pre-signed-upload-url/index.js
@@ -3,11 +3,9 @@ const s3 = new AWS.S3()
 
 exports.handler = async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-  console.log("EVENT: \n" + JSON.stringify(context, null, 2));
+  console.log("CONTEXT: \n" + JSON.stringify(context, null, 2));
   var contentType = event.headers['content-type'] !== undefined ? event.headers['content-type'] : 'application/octet-stream';
   var bucket = process.env.S3_BUCKET
   var params = {Bucket: bucket, Key: 'key.png', ContentType: contentType,};
-  var presignedUrl = s3.getSignedUrl('putObject', params);
-  console.log(presignedUrl);
-  return presignedUrl;
+  return s3.getSignedUrl('putObject', params);
 }
